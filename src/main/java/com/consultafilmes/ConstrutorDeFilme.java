e7a9dc08b9291ebdc6c1853daac652d81c139d2d
Lucas da Silva <lucassouza00012345@gmail.com>
2023-08-09 17:02:22 -0300

    chore: remove pasta de testes e da trybe

--- src/main/java/com/consultafilmes/ConstrutorDeFilme.java
+++ src/main/java/com/consultafilmes/ConstrutorDeFilme.java
@@ -1,4 +1,4 @@
-package com.trybe.consultafilmes;
+package com.consultafilmes;
 
 import java.util.Collections;
 import java.util.HashMap;
--- src/main/java/com/consultafilmes/Consultas.java
+++ src/main/java/com/consultafilmes/Consultas.java
@@ -1,4 +1,4 @@
-package com.trybe.consultafilmes;
+package com.consultafilmes;
 
 import java.util.AbstractMap;
 import java.util.Collection;
